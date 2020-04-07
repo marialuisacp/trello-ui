2e120c724b2b145d0da76b1efdd4a2d3b474e006
Maria Luisa <maria.luisa@thoughtworks.com>
2020-04-06 23:07:55 -0300

    [@marialuisacp] - Remove unnecessary code

--- components/card/Card.jsx
+++ components/card/Card.jsx
@@ -8,23 +8,20 @@ import PropTypes from 'prop-types';
 import Label from '../label/Label';
 import CardItemsIcons from '../cardItemsIcons/CardItemsIcons';
 
-const Card = props => {
-  if (props.cover)
-    console.log(props.cover);
-  return <StyledCard>
-    <StyledIconEdit className='icon-edit'></StyledIconEdit>
-    {props.cover && <StyledCardCover src={`/assets/${props.cover}.jpg`}></StyledCardCover>}
-    <div style={{ display: 'flex' }}>
-      {props.labels && props.labels.map((color, key) =>
-        <Label color={color} key={key}></Label>
-      )}
-    </div>
-    <StyledContentCard>
-      <StyledCardText>{props.text}</StyledCardText>
-    </StyledContentCard>
-    <CardItemsIcons icons={props.icons}></CardItemsIcons>
-  </StyledCard >
-};
+const Card = props => (<StyledCard>
+  <StyledIconEdit className='icon-edit'></StyledIconEdit>
+  {props.cover && <StyledCardCover src={`/assets/${props.cover}.jpg`}></StyledCardCover>}
+  <div style={{ display: 'flex' }}>
+    {props.labels && props.labels.map((color, key) =>
+      <Label color={color} key={key}></Label>
+    )}
+  </div>
+  <StyledContentCard>
+    <StyledCardText>{props.text}</StyledCardText>
+  </StyledContentCard>
+  <CardItemsIcons icons={props.icons}></CardItemsIcons>
+</StyledCard >
+);
 
 Card.propTypes = {
   text: PropTypes.string.isRequired,
